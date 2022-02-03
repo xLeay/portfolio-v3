c3021cd8f6304b0f33d6e95b94448191632bac77
Yaël <larkyriver@gmail.com>
2022-02-03 11:59:30 +0100

    Avancement Homepage 2

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ import { illusory } from 'illusory'
 // const end = document.querySelector('#to')
 
 // illusory(start, end)
+
+const hg = document.getElementById('hg');
+
+let sl = 0;
+
+function scroll() {
+    sl = sl + 2;
+    if (sl >= hg.scrollWidth/2 - 1) {
+        sl = 0;
+    }
+    hg.scrollLeft = sl;
+
+    window.requestAnimationFrame(scroll);
+}
+
+window.requestAnimationFrame(scroll);
