c5f0ed65d933cb4c72b17326c28d0a9a9faa866a
xLeay <LarkyRiver@gmail.com>
2022-02-05 00:16:27 +0100

    Un peu d'animation encore

--- index.js
+++ index.js
@@ -2,21 +2,16 @@ import { illusory } from 'illusory'
 
 // const start = document.querySelector('#from')
 // const end = document.querySelector('#to')
-
 // illusory(start, end)
 
 const hg = document.getElementById('movetext');
-
 let sl = 0;
-
 function scroll() {
-    sl = sl + 3;
+    sl = sl + 1;
     if (sl >= hg.scrollWidth/2 - 1) {
         sl = 0;
     }
     hg.scrollLeft = sl;
-
     window.requestAnimationFrame(scroll);
 }
-
 window.requestAnimationFrame(scroll);
