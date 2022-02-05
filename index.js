6467aa92ac09e0ba35d9f1aeafda1dfeb0065fae
Yaël <larkyriver@gmail.com>
2022-02-05 13:48:24 +0100

    Anim en plus

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ function scroll() {
     window.requestAnimationFrame(scroll);
 }
 window.requestAnimationFrame(scroll);
+
+
+const nav = document.querySelector("header")
+window.addEventListener('scroll', e => {
+    if (window.scrollY > 80) {
+        nav.classList.add("scrolled")
+    } else {
+        nav.classList.remove("scrolled")
+    }
+})
