ffab05b8f5f08f4645440f01cd36e53f77166496
Yaël <larkyriver@gmail.com>
2022-02-09 16:15:18 +0100

    Anim illusory en plus

--- index.js
+++ index.js
@@ -17,12 +17,44 @@ function scroll() {
 window.requestAnimationFrame(scroll);
 
 const nav = document.querySelector("header")
+
+var scrolled = false;
+var lastScrolled = scrolled;
+
 window.addEventListener('scroll', e => {
+
     if (window.scrollY > 80) {
-        
+        scrolled = true;
         nav.classList.add("scrolled")
     } else {
         nav.classList.remove("scrolled")
+        scrolled = false;
     }
+
+    const start = document.querySelector('#from')
+    const end = document.querySelector('#to')
+    
+    if (scrolled != lastScrolled) {
+        if (scrolled) {
+            setTimeout(() => {
+                illusory(start, end, {
+                    duration: '.5s'
+                })
+            }, 20);
+        } else {
+            setTimeout(() => {
+                illusory(end, start, {
+                    duration: '.5s'
+                })
+            }, 100);
+        }
+    }
+
+    lastScrolled = scrolled;
 })
 
+document.querySelector("#debug").addEventListener('click', () => {
+    const start = document.querySelector('#from')
+    const end = document.querySelector('#to')
+    illusory(start, end)
+});
