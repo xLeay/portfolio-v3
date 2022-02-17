5f5e31f34353d932aecad3ee742cd4a03baaa877
Yaël <larkyriver@gmail.com>
2022-02-17 12:14:19 +0100

    snap scroll début

--- work/work.js
+++ work/work.js
@@ -46,58 +46,58 @@ window.addEventListener('scroll', e => {
 
 
 /* IMAGE SLIDER */ /* IMAGE SLIDER */ /* IMAGE SLIDER */ /* IMAGE SLIDER */ /* IMAGE SLIDER */
-const items = document.querySelectorAll('img');
-const nbSlide = items.length;
-const suivant = document.querySelector('.right');
-const precedent = document.querySelector('.left');
-let count = 0;
+// const items = document.querySelectorAll('img');
+// const nbSlide = items.length;
+// const suivant = document.querySelector('.right');
+// const precedent = document.querySelector('.left');
+// let count = 0;
 
-function slideSuivante() {
-
-    
-    items[count].classList.remove('active');
-
-    if (count < nbSlide - 1) {
-        count++;
-    } else {
-        count = 0;
-    }
-
-    items[count].classList.add('active');
-
-    items[count].style.animation = "fadein 5s cubic-bezier(.38,.09,.24,1.08) 1 both";
-}
-suivant.addEventListener('click', slideSuivante);
-
-
-function slidePrecedente(){
+// function slideSuivante() {
 
     
-    items[count].classList.remove('active');
+//     items[count].classList.remove('active');
 
-    if(count > 0){
-        count--;
-    } else {
-        count = nbSlide - 1;
-    }
+//     if (count < nbSlide - 1) {
+//         count++;
+//     } else {
+//         count = 0;
+//     }
 
-    items[count].classList.add('active');
+//     items[count].classList.add('active');
 
-    items[count].style.animation = "fadeout 5s cubic-bezier(.38,.09,.24,1.08) 1 both";
+//     items[count].style.animation = "fadein 5s cubic-bezier(.38,.09,.24,1.08) 1 both";
+// }
+// suivant.addEventListener('click', slideSuivante);
 
-}
-precedent.addEventListener('click', slidePrecedente);
 
-function keyPress(e) {
-    if (e.keyCode === 37) {
-        slidePrecedente();
-    } else if (e.keyCode === 39) {
-        slideSuivante();
-    }
-}
+// function slidePrecedente(){
 
-document.addEventListener('keydown', keyPress);
+    
+//     items[count].classList.remove('active');
 
-setInterval(slideSuivante, 5000);
+//     if(count > 0){
+//         count--;
+//     } else {
+//         count = nbSlide - 1;
+//     }
+
+//     items[count].classList.add('active');
+
+//     items[count].style.animation = "fadeout 5s cubic-bezier(.38,.09,.24,1.08) 1 both";
+
+// }
+// precedent.addEventListener('click', slidePrecedente);
+
+// function keyPress(e) {
+//     if (e.keyCode === 37) {
+//         slidePrecedente();
+//     } else if (e.keyCode === 39) {
+//         slideSuivante();
+//     }
+// }
+
+// document.addEventListener('keydown', keyPress);
+
+// setInterval(slideSuivante, 5000);
 /* IMAGE SLIDER */ /* IMAGE SLIDER */ /* IMAGE SLIDER */ /* IMAGE SLIDER */ /* IMAGE SLIDER */
 
